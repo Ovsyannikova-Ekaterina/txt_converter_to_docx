fe0c23219fdfcea1933905ee4aa4d13d9d6be337
Kate <xilatomil@gmail.com>
2025-11-24 01:06:28 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from docx import Document
+
+
+def txt_to_word(input_file, output_file):
+    with open(input_file, 'r', encodind='utf-8') as txt_file:
+        text = txt_file.read()
+    doc = Document()
+    doc.add_paragraph(text)
+    doc.save(output_file)
+
+
+input_txt_file = 'input.txt'
+output_word_file = 'output.docx'
+txt_to_word(input_txt_file, output_word_file)
